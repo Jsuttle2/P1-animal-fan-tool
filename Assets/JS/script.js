af18f5a00ce8848229a45b2f2d3217f5499274c0
Tiffany <tapintobeauty08@gmail.com>
2023-03-21 14:53:01 -0400

    drop down list commit

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,19 @@
-var searchInput = 
 
 
 
 fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDcDNFqw79rNBAcjg0nY-N21UfKqVW4U3s&part=snippet&q=koalas&type=video')
-  .then(response => response.json())
-  .then(data => {
-    // Parse the response data and display the results on your website
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+.then(response => response.json())
+.then(data => {
+  // Parse the response data and display the results on your website
+  console.log(data);
+})
+.catch(error => {
+  console.error(error);
+});
+
+//var searchInput = 
+var animals = ["animal 1", "animal 2", "animal 3", "animal 4", "animal 5", "animal 6"];
+var animalSelection = document.querySelector("[]");
+var apiURL = urlStart + animals[selectedoption] + urlEnd;
+
 

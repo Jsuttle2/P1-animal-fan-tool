7955107acd64ae9834d939d19e9e65fb17464699
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-03-22 14:36:18 -0400

    Fetch and display img, title, desc, etc.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,16 @@
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split("&");
-  console.log(searchParamsArr);
-  // Get the query and format values
-  var query = searchParamsArr[0].split("=").pop();
-  var format = searchParamsArr[1].split("=").pop();
+var animalNamesInput = document.querySelector('#animal-names');
+var cardParent = document.querySelector('#card-parent')
 
-  searchApi(query, format);
-}
+// function getParams() {
+//   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+//   var searchParamsArr = document.location.search.split("&");
+//   console.log(searchParamsArr);
+//   // Get the query and format values
+//   var query = searchParamsArr[0].split("=").pop();
+//   var format = searchParamsArr[1].split("=").pop();
+
+//   searchApi(query, format);
+// }
 
 function getAnimalOption(event) {
   console.log(event.target.value);
@@ -15,17 +18,58 @@ function getAnimalOption(event) {
   searchApi(event.target.value);
 }
 
+function printResults (dataResults) {
+  console.log(dataResults)
+  cardParent.classList.add('columns')
+
+  var resultCard = document.createElement('div')
+  resultCard.classList.add('card', 'column')
+  cardParent.appendChild(resultCard)
+
+  var resultImgHold = document.createElement('div')
+  resultImgHold.classList.add('card-image', 'is-4by3', 'image')
+  resultCard.appendChild(resultImgHold)
+
+  var resultCardCon = document.createElement('div')
+  resultCardCon.classList.add('card-content')
+  resultCard.appendChild(resultCardCon)
+
+  var thumbnailImg = document.createElement('img')
+  thumbnailImg.setAttribute('src', dataResults.snippet.thumbnails.high.url)
+  thumbnailImg.setAttribute('alt', dataResults.snippet.title)  
+  resultImgHold.appendChild(thumbnailImg)
+
+  var titleEl = document.createElement('p')
+  titleEl.classList.add('title', 'is-4')
+  titleEl.textContent = dataResults.snippet.title
+  resultCardCon.appendChild(titleEl)
+
+  var cardDisc = document.createElement('div')
+  cardDisc.classList.add('content')
+  cardDisc.textContent = dataResults.snippet.description
+  resultCard.appendChild(cardDisc)
+
+
+
+
+}
+
 function searchApi(value) {
   var locQueryUrl =
-    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDcDNFqw79rNBAcjg0nY-N21UfKqVW4U3s&part=snippet&q=" +
+    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBHegcloyyeTcXZ7yRN4vubnnbqYIIJ8vE&part=snippet&q=" +
     value +
-    "&type-video";
+    "&type-video&VideoCategoryId=15";
 
   fetch(locQueryUrl)
     .then((response) => response.json())
     .then((data) => {
-      // Parse the response data and display the results on your website
-      console.log(data);
+      // console.log(data);
+      
+      for (let i = 0; i < data.items.length; i++) {
+        printResults(data.items[i])
+        
+      }
+      
     })
     .catch((error) => {
       console.error(error);
@@ -37,5 +81,5 @@ function searchApi(value) {
 // var animalSelection = document.querySelector("[]");
 // var apiURL = urlStart + animals[selectedoption] + urlEnd;
 
-var animalNamesInput = document.querySelector('#animal-names');
+
 animalNamesInput.addEventListener('change', getAnimalOption);

d405535377f70be4b527a0ed9bb75e3056f40182
Sarah~ <s.k.miller422@gmail.com>
2023-03-22 12:41:05 -0400

    created a function called getAnimalOptions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,19 +1,41 @@
+function getParams() {
+  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+  var searchParamsArr = document.location.search.split("&");
+  console.log(searchParamsArr);
+  // Get the query and format values
+  var query = searchParamsArr[0].split("=").pop();
+  var format = searchParamsArr[1].split("=").pop();
 
+  searchApi(query, format);
+}
 
+function getAnimalOption(event) {
+  console.log(event.target.value);
+  // START
+  searchApi(event.target.value);
+}
 
-fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDcDNFqw79rNBAcjg0nY-N21UfKqVW4U3s&part=snippet&q=koalas&type=video')
-.then(response => response.json())
-.then(data => {
-  // Parse the response data and display the results on your website
-  console.log(data);
-})
-.catch(error => {
-  console.error(error);
-});
+function searchApi(value) {
+  var locQueryUrl =
+    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDcDNFqw79rNBAcjg0nY-N21UfKqVW4U3s&part=snippet&q=" +
+    value +
+    "&type-video";
 
-//var searchInput = 
-var animals = ["animal 1", "animal 2", "animal 3", "animal 4", "animal 5", "animal 6"];
-var animalSelection = document.querySelector("[]");
-var apiURL = urlStart + animals[selectedoption] + urlEnd;
+  fetch(locQueryUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Parse the response data and display the results on your website
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
+//var searchInput =
+// var animals = ["animal 1", "animal 2", "animal 3", "animal 4", "animal 5", "animal 6"];
+// var animalSelection = document.querySelector("[]");
+// var apiURL = urlStart + animals[selectedoption] + urlEnd;
 
+var animalNamesInput = document.querySelector('#animal-names');
+animalNamesInput.addEventListener('change', getAnimalOption);
